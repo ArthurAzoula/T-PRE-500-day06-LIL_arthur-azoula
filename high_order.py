68c206bd4f62ab540940f88cc82ec8f6348f8dcd
Arthur Azoula <arthur.azoula@epitech.eu>
2023-09-18 12:08:40 +0200

    Finish day 06

--- high_order.py
+++ high_order.py
@@ -5,7 +5,8 @@ def funA(s: str, n: int):
     for char in s:
         if char.islower():
             count += 1
-    return n == count
+    res = True if n == count else f'/!\ Error : contains {count} lower characters, check for {n} lower characters'
+    return res
 
 print(funA("Oui", 2))
 
@@ -14,12 +15,13 @@ def funB(s: str, n: int):
     for char in s:
         if char.isupper():
             count += 1
-    return n == count
+    res = True if n == count else f'/!\ Error : contains {count} upper characters, check for {n} upper characters'
+    return res
 
 print(funB("Oui", 1))
 
 def funC(s: str, n: int):
-    return len(s) == n
+    return True if n == len(s) else f'/!\ Error : contains {len(s)} characters, check for {n} characters'
 
 print(funC('Oui', 3))
 
@@ -28,7 +30,8 @@ def funD(s: str, n:int):
     for char in s:
         if not char.isalpha():
             count += 1
-    return count == n
+    res = True if n == count else f'/!\ Error : contains {count} specials characters, check for {n} specials characters'
+    return res
 
 print(funD('Oui%¹^!:', 5))
 
@@ -37,15 +40,16 @@ def funE(s: str, n: int):
     for char in s:
         if char in '0123456789':
             count += 1
-    return count == n
+    res = True if n == count else f'/!\ Error : contains {count} lower characters, check for {n} characters'
+    return res
 
 print(funE('1Oui3', 2))
 
-# task 01
+# task 01 and 02
 
-def check_password(command, count: int, password: str) -> bool:
+def check_password(command, count: int, password: str) -> bool or str:
     
     return command(password, count)
 
 
-print("Check password : ", check_password ( funA , 4 , " MyseZaJH " ) and check_password ( funD, 2, 'Mypass%¨'))
+print("Check password : ", check_password ( funA , 2, " MyseZaJH" ))

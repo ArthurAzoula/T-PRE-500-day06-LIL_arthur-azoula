581377bfa2e416580fb58342e92c1df78786eae5
Arthur Azoula <arthur.azoula@epitech.eu>
2023-09-18 16:50:23 +0200

    add recursif

--- challenge.py
+++ challenge.py
@@ -1,12 +1,18 @@
 import time
 
 def duration_power(nb, puissance):
-    start = time.time()
+    start = time.time_ns()
 
     nb**puissance
 
-    return time.time() - start
+    return time.time_ns() - start
 
 print(duration_power(42,84))
 
 print(duration_power(42, 168))
+
+def duration_power_recursif(nb, puissance):
+    
+    return nb * duration_power(nb, puissance - 1) if puissance >= 1 else 1
+    
+print(duration_power_recursif(42,168))
--- high_order.py
+++ high_order.py
@@ -5,7 +5,7 @@ def funA(s: str, n: int):
     for char in s:
         if char.islower():
             count += 1
-    res = True if n >= count else f'/!\ Error : contains {count} lower characters, check for {n} lower characters'
+    res = True if count >= n else ImportError(f'/!\ Error : contains {count} lower characters, check for {n} lower characters')
     return res
 
 print(funA("Oui", 2))
@@ -15,22 +15,22 @@ def funB(s: str, n: int):
     for char in s:
         if char.isupper():
             count += 1
-    res = True if n >= count else f'/!\ Error : contains {count} upper characters, check for {n} upper characters'
+    res = True if count >= n else ImportError(f'/!\ Error : contains {count} upper characters, check for {n} upper characters')
     return res
 
 print(funB("Oui", 1))
 
 def funC(s: str, n: int):
-    return True if n >= len(s) else f'/!\ Error : contains {len(s)} characters, check for {n} characters'
+    return True if len(s) >= n else ImportError(f'/!\ Error : contains {len(s)} characters, check for {n} characters')
 
 print(funC('Oui', 3))
 
 def funD(s: str, n:int):
     count = 0
     for char in s:
-        if not char.isalpha():
+        if not char.isalpha() and char not in '0123456789':
             count += 1
-    res = True if n >= count else f'/!\ Error : contains {count} specials characters, check for {n} specials characters'
+    res = True if count >= n else ImportError(f'/!\ Error : contains {count} specials characters, check for {n} specials characters')
     return res
 
 print(funD('Oui%¹^!:', 5))
@@ -40,16 +40,16 @@ def funE(s: str, n: int):
     for char in s:
         if char in '0123456789':
             count += 1
-    res = True if n >= count else f'/!\ Error : contains {count} lower characters, check for {n} characters'
+    res = True if count >= n else ImportError(f'/!\ Error : contains {count} lower characters, check for {n} characters')
     return res
 
 print(funE('1Oui3', 2))
 
 # task 01 and 02
 
-def check_password(command, count: int, password: str) -> bool or str:
+def check_password(command, count: int, password: str) -> bool or ImportError:
     
     return command(password, count)
 
 
-print("Check password : ", check_password ( funA , 2, " MyseZaJH" ))
+print("Check password : ", check_password ( funA , 4, " MyseZaJH" ))
